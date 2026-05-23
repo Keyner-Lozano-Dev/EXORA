6b3c63c3354b2604d9320414271e330971545584
 <keynerlozano2005@gmail.com>
2026-05-23 15:33:38 -0500

    se agregan funcionalidades a los botodes de DASHBOARD

--- main.php
+++ main.php
@@ -214,7 +214,7 @@ $con = connection();
                         <i class="fa-solid fa-calendar-days"></i> Dashboard
                     </a>
                     <a href="#" onclick="cargarSeccion('agenda', this); return false;">
-                        <i class="fa-solid fa-user-multi"></i> Clientes
+                        <i class="fa-solid fa-users"></i> Clientes
                     </a>
                     <a href="#" onclick="cargarSeccion('eventos', this); return false;">
                         <i class="fa-solid fa-clock"></i> Eventos
